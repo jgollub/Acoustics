84940e578bd6be930fafe169effba8306962a496
Jonah Gollub <jgollub@gmail.com>
2017-05-14 15:31:15 -0400

    cleaned up data import

--- Process Experimental Data/AcousticExpImage.m
+++ Process Experimental Data/AcousticExpImage.m
@@ -2,6 +2,48 @@
 %SAS - Acoustic Range Migration Algorithm 
 %J. Gollub
 %-------------------------------------------------------------------------------
+%% generate Chirp function
+
+
+phi=@(t) 2*pi*(f0*t+(f1-f0)/(2*tau).*t.^2);
+% 
+% chirp = @(t,phi) ((t<(T-tau)/2 | t>(T+tau)/2).*0 ...
+%                     +(t> (T-tau)/2 & t<=(T+tau)/2).*exp(1j.*phi));
+% 
+% phi=@(t,r) 2*pi*(f0*t+(f1-f0)/(2*tau).*t.^2);
+
+phi_r=@(t,r) -(2*pi/c)*(f0+(f1+f0)/(2*tau).*t).*r;
+
+
+chirp = @(t,phi) ((t<(T-tau)/2 | t>(T+tau)/2).*0 ...
+                    +(t> (T-tau)/2 & t<=(T+tau)/2).*exp(1j.*phi));
+
+
+chirp_r = @(t,phi,phi_r) ((t<(T-tau)/2 | t>(T+tau)/2).*0 ...
+                    +(t> (T-tau)/2 & t<=(T+tau)/2).*exp(1j.*phi)).*exp(1j.*phi_r);
+                
+                r=6;       
+cla;
+hold on;
+
+sig=chirp(t,phi(t));
+
+plot(t, imag(chirp_r(t-r/c,phi(t), phi_r(t-r/c,r))),'b')
+plot(t, imag(chirp_r(t-r/c,phi(t), phi_r(t,r))),'g')
+sig_reverse=conj(chirp(t-r/c,phi(t-r/c)));
+
+plot(t, imag(chirp(t-r/c,phi(t))),'b')
+plot(t, imag(conj(chirp(t-r/c,phi(t-r/c)))),'r')
+
+plot(t, abs(chirp(t-r/c,phi(t-r/c)).*conj(chirp(t-r/c,phi(t-r/c)))),'k')
+
+figure(102)
+       
+apollo=fft(real(chirp(t,phi(t))));
+plot(0:1/T:44.1E3/2-1/T, apollo(1:end/2),'r')
+
+                
+
 %% load data and set parameters
 c=343; % speed of sound m/s
 T=2.2;
@@ -87,18 +129,43 @@ t_tau=data.chirp.t_tau;
 chirp_sig=data.chirp.chirp_sig;
 tau=data.chirp.tau;
 
-%setup for fft to evaluate in frequency domain
-figure(51);
-subplot(2,1,1);
-plot(t,db(data.measurements{floor(end/2),floor(end/2)}(:,2)),t,db(data.measurements{floor(end/2),floor(end/2)}(:,1)))
-axis tight
-title('Signal, time domain (applied, measured)')
-xlabel('time (s)')
-ylabel('voltage (a.u.)')
+%plot chirp generated Vs defined chirp
+figure(1); clf; 
+subplot(1,3,1); hold on; 
+plot(t,real(chirp_sig));  plot(t,real(chirp(t,phi(t))),'--k'); xlim([0-.1,T+.1]); 
+title('Generated Chirp Vs Function')
+xlabel('t(s)'); ylabel('Amplitude (a.u.)')
+subplot(1,3,2:3); hold on; 
+plot(t,real(chirp_sig));  plot(t,real(chirp(t,phi(t))),'--k'); 
+xlim([T/2-tau/2-0.001,T/2-tau/2+0.001]); xlabel('t(s)'); 
+title('Excerpt')
 
-%time sampling
+%plot Measured Vs Generated chirp and Soundcard offsets
+figure(2); clf;
+subplot(3,1,1);
+plot(t,db(data.measurements{floor(end/2),floor(end/2)}(:,2)),t,db(data.measurements{floor(end/2),floor(end/2)}(:,1)))
+axis tight; xlabel('time (s)'); ylabel('db(voltage)');
+title('Signal, time domain (Generated, Measured)');
+
+subplot(3,1,2)
+test=fft(data.measurements{floor(end/2),floor(end/2)}(:,1));
+plot(freqs,db(test(1:floor(end/2))));
+axis tight; xlabel('Freq (Hz)'); ylabel('db(voltage)');
+title('Measured Signal in the Frequency Domain');
+
+subplot(3,1,3); cla; hold on;
+for i=1:3
+    hold on;
+    plot(t,squeeze(squeeze(data.measurements{floor(end/2)+i,floor(end/2)+i}(:,2))))
+end
+axis tight; xlabel('time (s)'); ylabel('voltage (a.u.)'); xlim([.254,0.265]);
+title('Input signals (Unexpected Offset)');
+
+
+%time sampling Will be updated in NFS script!!!
 freqs=0:1/T:(44.1E3/2)-1/T;
 
+%load data scan positions
 X=data.X/1000;  
 dx=X(1,2)-X(1,1);
 Lx=X(1,end)-X(1,1);
@@ -108,162 +175,82 @@ Y=data.Y/1000;
 dy=Y(2,1)-Y(1,1);
 Ly=Y(end,1)-Y(1,1);
 
-Experiment=cell(size(data.measurements));
+% Experiment=cell(size(data.measurements));
+% Experiment=cellfun(@(x) x(:,1), data.measurements,'UniformOutput', false);
+% ExperimentFFT=cell(size(data.measurements));
+% signal=cellfun(@(x) minus(x(:,1),y(:,1)),ExperimentFFT,backgroundFFT,'UniformOutput',false);
+% ExperimentFFT=cellfun(@fft,data.measurements, 'UniformOutput', false);
+% ExperimentFFT=cellfun(@(x) x(1:end/2,1),ExperimentFFT,'UniformOutput',false);
 
-
-
-
-Experiment=cellfun(@(x) x(:,1), data.measurements,'UniformOutput', false);
-
-figure()
-for i=1:10
-    hold on;
- plot(t,squeeze(squeeze(data.measurements{floor(end/2)+i,floor(end/2)+i}(:,2))))
- end
-
-
-
-% Experiment=cellfun(@(x) x(end-20000:1000:end-10000,1), data.measurements,'UniformOutput', false);
-
-% t=data.chirp.t;
-% t=t(end-20000:1000:end-10000); 
+%choose frequencies
+choosefreq=find(freqs>16000 & freqs<18900);
+choosefreq=choosefreq(1:100:end);
 
 measurements=zeros([size(Experiment),size(Experiment{1,1})]);
-
-
-reference=data.measurements{1,1}(:,2);
-for nx=1:size(Experiment,1)
-    for ny=1:size(Experiment,2)
-        [XcorrSign,pos]=max(xcorr(reference,data.measurements{nx,ny}(:,2)));
-        pos=pos-length(t);
-        if XcorrSign>=0
-            measurements(ny,nx,:)=Experiment{nx,ny}; %note order needs to match meshgrid
-        elseif XcorrSign<0
-            measurements(ny,nx,:)=-Experiment{nx,ny}; %note order needs to match meshgrid
-        end
-    end
-end
-
-ExperimentFFT=cell(size(data.measurements));
-ExperimentFFT=cellfun(@fft,data.measurements, 'UniformOutput', false);
-
-figure(51); 
-subplot(2,1,2)
-test=fft(data.measurements{floor(end/2),floor(end/2)}(:,1));
-plot(freqs,db(test(1:floor(end/2))));
-xlabel('db')
-
-% load(['D:\Dropbox (Duke Electric & Comp)\MetaImager Data\Acoustic Data\',...
-%                  'SAS of 16oz Can 5-1-2017\results-29-Apr-2017_BkGrnd.mat']);
-
-% backgroundFFT=cell(size(data.measurements));
-% backgroundFFT=cellfun(@fft,data.measurements, 'UniformOutput', false);
-
-% signal=cellfun(@(x) minus(x(:,1),y(:,1)),ExperimentFFT,backgroundFFT,'UniformOutput',false);
-ExperimentFFT=cellfun(@(x) x(1:end/2,1),ExperimentFFT,'UniformOutput',false);
-
-choosefreq=find(freqs>16000 & freqs<18900);
-choosefreq=choosefreq(1:10:end);
+measurement_t_offset=zeros(size(ExperimentFFT));
 measurementsfft=zeros([size(ExperimentFFT),length(choosefreq)]);
-  
 
-
-reference=data.measurements{1,1}(:,2);
+tRef=data.measurements{1,1}(:,2);
 temp=0;
-
-
-
+tic
 for nx=1:size(ExperimentFFT,1)
     for ny=1:size(ExperimentFFT,2)
-        [XcorrSign,indx]=max(xcorr(reference,data.measurements{nx,ny}(:,2)));
+        tData=data.measurements{nx,ny};
+       
+        %determine offset of singal w.r.t set reference measurement
+        [XcorrSign,indx]=max(xcorr(tRef,tData(:,2)));
         indx=indx-length(t);
+        measurements(ny,nx,:)=tData(:,1);
+        measurement_t_offset(nx,ny)=dt*indx;
+        
+        FFTtdata=fft(tData(:,1));
+        FFTtdata=FFTtdata(1:end/2);
         for nf=choosefreq
             temp=temp+1;
-                measurementsfft(ny,nx,temp)=exp(-1j*2*pi*freqs(nf)*dt*indx)*ExperimentFFT{nx,ny}(nf); %note order needs to match meshgrid
+            %note time shift is phase shift in frequency domain
+            measurementsfft(ny,nx,temp)=exp(-1j*2*pi*freqs(nf)*dt*indx)*FFTtdata(nf); %note order needs to match meshgrid
         end
         temp=0;
     end
 end
+toc
 
 f=freqs(choosefreq);
 BW=f(end)-f(1);
 
-figure(52); 
-
-% for i=1:length(f)
-% imagesc(X(1,:),Y(:,1),abs(measurementsfft(:,:,i)))
-% axis equal;
-% axis tight;
-% title(['freqs ', num2str(f(i))])
-% drawnow
-% pause(.1)
-% end
-
-
-%%probe correction???
-% 
-% figure(1);
-% clf; hold on;
-% plot(f,db(ExperimentFFT{floor(end/2),floor(end/2)}(1:end/2,1)),'-b');
-% plot(f,db(backgroundFFT{floor(end/2),floor(end/2)}(1:end/2,1)), '-r');
-% plot(f,db(signal{floor(end/2),floor(end/2)}(1:end/2,1)), '-k');
-% 
-% plot(f,db(squeeze(squeeze(measurements(floor(end/2),floor(end/2),:)))), '-k');
-
-% signal(:,1)
-% 
-% figure(1);
-% clf; hold on;
-% plot(f,db(sodaCanfft{50,50}(1:end/2,1)),'-b');
-% plot(f,db(backgroundfft{50,50}(1:end/2,1)), '-r');
-% 
-% plot(f,db(sodaCanfft{50,50}(1:end/2,1)...
-%     -backgroundfft{50,50}(1:end/2,1)), '-g');
-% 
-% plot(f,db(signal{50,50}(1:end/2,1)), '-k');
-
-% x_p=0.025;
-% y_p=0.1;
+%% Scan setup
 z_p=.3; 
 
 %  clear data;
-%% propagate fields
-
-figure(103)
-imagesc(X(1,:),Y(:,1),abs(measurementsfft(:,:,end-100)))
-axis equal;
-axis tight;
-title(['freqs ', num2str(f(i))])
-drawnow
-
+%% propagate fields single frequency
 nn=100;
 singleFreqData=measurementsfft(:,:,end-nn);
 singleFreq=f(end-nn);
 
-
-subplot(1,3,1)
+figure(10); clf;
+subplot(2,3,1)
 imagesc(X(1,:),Y(:,1),abs(singleFreqData)); axis equal; axis tight;
-
-title('Abs(P)')
-subplot(1,3,2)
+title('Abs(p)')
+subplot(2,3,2)
 imagesc(X(1,:),Y(:,1),real(singleFreqData)); axis equal; axis tight;
-title('Real(P)')
-subplot(1,3,3)
+title('Real(p)')
+subplot(2,3,3)
 imagesc(X(1,:),Y(:,1),imag(singleFreqData)); axis equal; axis tight;
+title('Imag(p)')
+hold on;
 
-title('Imag(P)')
+% figure(11)
+% for i=1:100
+%     if mod(i,2)
+%         imagesc(X(1,:),Y(:,1),real(singleFreqData).^2); axis equal; axis tight;
+%         pause(.05)
+%     elseif ~mod(i,2)
+%         imagesc(X(1,:),Y(:,1),imag(singleFreqData).^2); axis equal; axis tight;
+%         pause(.05)
+%     end
+% end
 
-for i=1:100
-    if mod(i,2)
-        imagesc(X(1,:),Y(:,1),real(singleFreqData).^2); axis equal; axis tight;
-        pause(.05)
-    elseif ~mod(i,2)
-        imagesc(X(1,:),Y(:,1),imag(singleFreqData).^2); axis equal; axis tight;
-        pause(.05)
-    end
-end
 
-figure(104)
 pad=2^nextpow2(max(size(singleFreqData))+1);
 [kx,ky]=meshgrid(linspace(-2*pi/(2*dx),2*pi/(2*dx),pad),linspace(-2*pi/(2*dy),2*pi/(2*dy),pad));
 
@@ -273,17 +260,23 @@ shifty=dy*(pad/2-ceil(ynum/2));
 Ekxky=circshift(fft2(singleFreqData, pad, pad),[pad/2,pad/2]);
 Ekxky=Ekxky.*exp(-1.0j*kx*(shiftx)).*exp(-1.0j*ky*(shifty));
 
-imagesc(abs(Ekxky))
+subplot(2,3,4)
+imagesc(X(1,:),Y(:,1), abs(Ekxky)); axis tight; axis equal; axis xy;
+title(['k-space af f= ',num2str(singleFreq)]);
+title('K-Space Abs(P)')
+subplot(2,3,5)
+imagesc(X(1,:),Y(:,1), real(Ekxky)); axis tight; axis equal; axis xy;
+title('K-Space Real(P)')
 
-
-% [kx,ky]=meshgrid(linspace(-2*pi/(2*dx),2*pi/(2*dx),pad),linspace(-2*pi/(2*dy),2*pi/(2*dy),pad));
-%  [kx,ky]=meshgrid(-2*pi/(2*dx):2*pi/Lx:(2*pi/(2*dx)),-2*pi/(2*dy):2*pi/Ly:(2*pi/(2*dy)));
-% [kx,ky]=meshgrid(-2*pi/(2*dx):2*pi/Lx:(2*pi/(2*dx)),-2*pi/(2*dy):2*pi/Ly:(2*pi/(2*dy)));
+subplot(2,3,6)
+imagesc(X(1,:),Y(:,1), imag(Ekxky)); axis tight; axis equal; axis xy;
+title('K-Space Imag(P)')
 
 k0=2*pi*singleFreq/c
 kzs=real(sqrt(k0.^2-kx.^2-ky.^2));
+
 for zd=0:-.005:-.6
-Exy=(ifft2(fftshift(Ekxky.*exp(-1j*kzs*zd))));
+Exy=(ifft2(fftshift(Ekxky.*exp(-1j*kzs*zd)))); %why neg sign ??????
 
 imagesc(abs(Exy))
 axis equal; axis tight;
@@ -291,7 +284,6 @@ title(['xd= ', num2str(zd)])
 drawnow
 pause(.2)
 end
-
 
 %% Phase reversal technique
 %positions
@@ -356,7 +348,6 @@ Ly_pad=dy*(pad-1);
                      linspace(-(2*pi)/(2*dy),(2*pi)/(2*dy),pad), ...
                      linspace(0,f(end),size(measurementsfft,3)));
 
-
 %calc free space k vector
 % k=2*pi*f/c;
 k=linspace(0,2*pi*f(end)/c,size(measurementsfft,3));
@@ -379,7 +370,7 @@ shifty=dy*(pad/2-ceil(ynum/2));
 Sxy=Sxy.*exp(-1.0j*kux*(shiftx)).*exp(-1.0j*kuy*(shifty));
 
     %plot decomposition
-    figHandle=figure(1);
+    figHandle=figure(20);
     scrn = get( groot, 'Screensize'); scrn(1)=2*scrn(3)/3;  scrn(3)=scrn(3)/3;
     set(figHandle,'Position',scrn); clf; subplot(2,2,1);
     imagesc(-Lx_pad/2:dx:Lx_pad/2,-Ly_pad/2:dy:Ly_pad/2,abs(ifft2(ifftshift(Sxy(:,:,60)))));
@@ -449,7 +440,7 @@ dky = mean(diff(linspace(-pi/dy, pi/dy, size(Srmg,2))));
 yy = linspace(-pi/dky,pi/dky,size(image,2));
 
 %%%
-    figure(1); subplot(2,1,2); cla;
+    figure(20); subplot(2,1,2); cla;
     set(gcf,'color','white'); colormap('parula');
     vol3d('Cdata',image.^4,'xdata',xx,'Ydata',yy,'Zdata',zz);
     axis equal; axis tight; view(3);
@@ -465,7 +456,7 @@ zmax=6;
 
 subimage=image(yy>ymin & yy<ymax, xx>xmin & xx<xmax, zz>zmin & zz<zmax);
 % subimage=subimage/max(subimage(:));
-    figure(1); subplot(2,1,2); cla;
+    figure(20); subplot(2,1,2); cla;
     set(gcf,'color','white'); colormap('parula');
     vol3d('Cdata',subimage.^6,...
         'xdata',xx(xx>xmin & xx<xmax),...
